2ac4e5f2f9e3b3f68091d3eb4ab23a63639453d9
Nahuel Aguirre <167265415+nahuelaguirre74@users.noreply.github.com>
2024-10-06 19:14:56 -0600

    Inclusión de Header, Body y Footer

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import React, { useState } from 'react';
-import Login from './components/Login';
-import Genre from './components/Genre';
-import BookList from './components/BookList';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Body from './components/Body';
 import './App.css';
 
-const App = () => {
-  <Header/>
-  
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [selectedGenre, setSelectedGenre] = useState(null);
-
-  const genres = ['Latinoamérica', 'Terror', 'Ciencia Ficción', 'Biografía'];
-  const books = [
-    { title: 'Libro uno', author: 'Autor' },
-    { title: 'Libro dos', author: 'Autor' },
-  ];
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleSelectGenre = (genre) => {
-    setSelectedGenre(genre);
-  };
-
+function App() {
   return (
-    
     <div>
-      
-      
-      {!isLoggedIn ? (
-        <Login onLogin={handleLogin} />
-      ) : !selectedGenre ? (
-        <Genre genres={genres} onSelectGenre={handleSelectGenre} />
-      ) : (
-        <BookList genre={selectedGenre} books={books} />
-      )}
-     
+      <Header />
+      <Body />
+      <Footer />
     </div>
-    
   );
-
-  
-  <Footer/>
-};
-
-
+}
 
 export default App;
